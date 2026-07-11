4c6478b80f891b353a8ad69c402638ccb4fce94f
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-11 08:30:15 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,13 @@ from app.database import connect_to_mongo, close_mongo_connection
 from bot.bot import app_bot
 from app.routes.download import router as download_router # <-- ADD THIS IMPORT
 
+# Insert this statement inside app/main.py along with other imports:
+from fastapi.staticfiles import StaticFiles
+
+# Add this command right right above app.include_router(download_router):
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 logger = logging.getLogger(__name__)
 
 @asynccontextmanager
